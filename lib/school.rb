78c6af27abd77f3d1c91ec5431ded3c73bc674f0
Leah Schlackman <leahemily1@gmail.com>
2020-09-15 23:32:49 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,35 @@
-# code here!
+require 'pry'
+class School
+  
+  def initialize(name)
+    @name = name
+    @roster = { }
+  end
+  
+  def roster 
+    @roster
+  end
+
+def add_student(student_name, grade)
+  # binding.pry
+  # @roster
+   if !@roster[grade] 
+    # == nil ![ ]
+     @roster[grade] = [ ]
+    # @roster[grade] << student_name
+  # else
+  end
+   @roster[grade] << student_name
+  end
+   
+   
+  def grade(grade)
+    @roster[grade]
+  end 
+  
+  def sort
+    @roster.each do |grade, students| 
+      students.sort!
+  end
+end
+end
